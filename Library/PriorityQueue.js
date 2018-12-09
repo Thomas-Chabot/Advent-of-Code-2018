c1d32baa6c1fa601718e2bade3c8e7c99b091fe2
Thomas-Chabot <seanhodges@cmail.carleton.ca>
2018-12-09 12:36:08 -0500

    added priority queue to libraries

---
+++ Library/PriorityQueue.js
@@ -0,0 +1,69 @@
+/* Priority Queue Implementation - from https://stackoverflow.com/questions/42919469/efficient-way-to-implement-priority-queue-in-javascript */
+let queueTop = 0;
+let parent = i => ((i + 1) >>> 1) - 1;
+let left = i => (i << 1) + 1;
+let right = i => (i + 1) << 1;
+
+class PriorityQueue {
+  constructor(comparator = (a, b) => a > b) {
+    this._heap = [];
+    this._comparator = comparator;
+  }
+  size() {
+    return this._heap.length;
+  }
+  isEmpty() {
+    return this.size() == 0;
+  }
+  peek() {
+    return this._heap[queueTop];
+  }
+  push(...values) {
+	for (let value of values){
+      this._heap.push(value);
+      this._siftUp();
+    }
+    return this.size();
+  }
+  pop() {
+    const poppedValue = this.peek();
+    const bottom = this.size() - 1;
+    if (bottom > queueTop) {
+      this._swap(queueTop, bottom);
+    }
+    this._heap.pop();
+    this._siftDown();
+    return poppedValue;
+  }
+  replace(value) {
+    const replacedValue = this.peek();
+    this._heap[queueTop] = value;
+    this._siftDown();
+    return replacedValue;
+  }
+  toString(){ return this._heap.join(", ") };
+  _greater(i, j) {
+    return this._comparator(this._heap[i], this._heap[j]);
+  }
+  _swap(i, j) {
+    [this._heap[i], this._heap[j]] = [this._heap[j], this._heap[i]];
+  }
+  _siftUp() {
+    let node = this.size() - 1;
+    while (node > queueTop && this._greater(node, parent(node))) {
+      this._swap(node, parent(node));
+      node = parent(node);
+    }
+  }
+  _siftDown() {
+    let node = queueTop;
+    while (
+      (left(node) < this.size() && this._greater(left(node), node)) ||
+      (right(node) < this.size() && this._greater(right(node), node))
+    ) {
+      let maxChild = (right(node) < this.size() && this._greater(right(node), left(node))) ? right(node) : left(node);
+      this._swap(node, maxChild);
+      node = maxChild;
+    }
+  }
+}
