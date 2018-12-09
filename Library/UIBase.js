442b0814209f57adf2258f2ce7b363e53ce51720
Thomas-Chabot <seanhodges@cmail.carleton.ca>
2018-12-09 12:33:06 -0500

    updated directories; added library directory

---
+++ Library/UIBase.js
@@ -0,0 +1,41 @@
+class UIBase {
+	/* Constructor */
+	constructor(text, splitter){
+		if (spliter === undefined) splitter = "\n";
+		let lines = text.trim().split(splitter);
+		this._data = this._construct(lines);
+	}
+	
+	/* Public Methods */
+	run(){
+		this._each((d)=>this._run(d));
+	}
+	
+	/* Private Methods */
+	// construct the object from the UI
+	_construct(lines){
+		let result = [ ];
+		for (var i = 0; i < lines.length; i++) {
+			result.push(this._parse(lines[i], i));
+		}
+		return result;
+	}
+	
+	/* Helper Methods */
+	_each(f){
+		for (var data of this._data)
+			f(data);
+	}
+	_sort(comparator){
+		//console.log("sorting")
+		this._data.sort(comparator);
+	}
+	
+	/* Must Overload */
+	_run(data){
+		throw new Error("_run method must be overloaded");
+	}
+	_parse(line, lineNumber){
+		throw new Error("_parse method must be overloaded");
+	}
+}
