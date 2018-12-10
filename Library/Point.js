0c2540270ce7acf9086ba556a41b893a72c594e0
Thomas <seanhodges@cmail.carleton.ca>
2018-12-10 16:04:11 -0500

    Create Point.js
    Base class for a point with (x, y) coordinates

---
+++ Library/Point.js
@@ -0,0 +1,22 @@
+class Point {
+	constructor(x, y) {
+		this._x = x;
+		this._y = y;
+	}
+	
+	get x(){ return this._x; }
+	get y(){ return this._y; }
+	
+	add(otherPoint){
+		return new Point(this.x + otherPoint.x, this.y + otherPoint.y, this.id);
+	}
+	
+	isLessThan(otherPoint){
+		return this.x < otherPoint.x || this.y < otherPoint.y;
+	}
+	isGreaterThan(otherPoint){
+		return this.x > otherPoint.x || this.y > otherPoint.y;
+	}
+	
+	toString(){ return `(${this.x}, ${this.y})`; }
+}
