3125aad5809afcb2ba9d39036f93239018e6d881
Thomas <seanhodges@cmail.carleton.ca>
2018-12-10 16:30:26 -0500

    Update Grid.js
    Removed reliance on Point object & added toString() method

--- Library/Grid.js
+++ Library/Grid.js
@@ -1,47 +1,63 @@
-// NOTE: Grid class relies on the Point class for get & set operations
 class Grid {
-	constructor(numRows, numColumns, defaultValue){
+	constructor(numRows, numColumns, default){
 		this._grid = [ ];
 		
 		this._numRows = numRows;
 		this._numColumns = numColumns;
-		this._defaultValue = defaultValue;
+		this._defaultValue = default;
 		
 		this._init();
 	}
 	
-	get(position){
-		return this._takeAction(position, (column)=>{
-			return column[position.x];
+	get(row, col){
+		return this._takeAction(row, col, (column)=>{
+			return column[row];
 		});
 	}
-	set(position, data){
-		return this._takeAction(position, (column)=>{
-			column[position.x] = data;
+	set(row, col, data){
+		return this._takeAction(row, col, (column)=>{
+			column[row] = data;
 			return true;
 		});
 	}
-	reset(position){
-		return this._set(position, this._defaultValue);
+	reset(row, col){
+		return this._set(row, col, this._defaultValue);
+	}
+	
+	toString(){
+		let str = "";
+		this._each((row, col, isNextColumn)=>{
+			if (isNextColumn)
+				str = str + "\n";
+			str = str + " " + this.get(row, col);
+		});
+		return str;
 	}
 	
 	_init(){
+		this._each((row,col,isNextColumn)=>{
+			if (isNextColumn)
+				this._grid[col] = [ ];
+			this._grid[col][row] = this._defaultValue;
+		});
+	}
+	
+	_each(f){
 		let nRows = this._numRows;
 		let nCols = this._numColumns;
 		
 		for (let col = 0; col < nCols; col++){
-			let column = [ ];
-			for (let row = 0; row < nRows; row++){
-				column[row] = this._defaultValue;
+			f(0, col, true);
+			for (let row = 1; row < nRows; row++){
+				f(row, col, false);
 			}
-			this._grid[col] = column;
 		}
 	}
 	
-	_takeAction(position, f) {
-		if (!this._borderCheck(position)) return false;
+	_takeAction(row, col, f) {
+		if (!this._borderCheck(row, col)) return false;
 	
-		let column = this._column(position.y);
+		let column = this._column(col);
 		if (!column) return false;
 		
 		return f(column);
@@ -52,9 +68,8 @@ class Grid {
 		
 		return this._grid[column];
 	}
-	
-	_borderCheck(position){
-		return (position.x >= 0 && position.x <= this._numRows
-			 && position.y >= 0 && position.y <= this._numColumns);
+	_borderCheck(row, col){
+		return (row >= 0 && row <= this._numRows
+			 && col >= 0 && col <= this._numColumns);
 	}
 }
