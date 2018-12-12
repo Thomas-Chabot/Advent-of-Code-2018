fd209f9064d776217405a30f9baee7a67b89a5cb
Thomas <seanhodges@cmail.carleton.ca>
2018-12-12 14:54:25 -0500

    Update Day12 - Solvable Part 2
    Anticipating the solution (when the increment value is repeating) to calculate the final result

--- Days/Day12.js
+++ Days/Day12.js
@@ -1,12 +1,11 @@
-/*
-  This is an implementation which works for Part 1 without any attempts at achieving better efficiency.
-   It will be much too slow to run on Part 2, so will need to work on efficiency for that
-*/
-
 /* Constants */
 const PLANT_POT = "#";
 const EMPTY_POT = ".";
-const NUM_ITERATIONS = 20;
+
+const NUM_ITERATIONS_P1 = 20;
+const NUM_ITERATIONS_P2 = 50 * 1000 * 1000 * 1000;
+
+const NUM_REPETITIONS = 5;
 
 const DEBUG = false;
 
@@ -87,18 +86,89 @@ class Trie {
 	}
 }
 
+/* Creates a new queue. A queue is a first-in-first-out (FIFO) data structure -
+ * items are added to the end of the queue and removed from the front.
+ */
+function Queue(){
+
+  // initialise the queue and offset
+  var queue  = [];
+  var offset = 0;
+
+  // Returns the length of the queue.
+  this.getLength = function(){
+    return (queue.length - offset);
+  }
+
+  // Returns true if the queue is empty, and false otherwise.
+  this.isEmpty = function(){
+    return (queue.length == 0);
+  }
+
+  // Returns true if all values are the same
+  this.isEquivalent = function(){
+	let value = queue[offset];
+	for (let item = offset; item < offset + queue.length; item++){
+		if (queue[item] !== queue[offset])
+			return false;
+	}
+	return true;
+  }
+  
+  /* Enqueues the specified item. The parameter is:
+   *
+   * item - the item to enqueue
+   */
+  this.enqueue = function(item){
+    queue.push(item);
+  }
+
+  /* Dequeues an item and returns it. If the queue is empty, the value
+   * 'undefined' is returned.
+   */
+  this.dequeue = function(){
+
+    // if the queue is empty, return immediately
+    if (queue.length == 0) return undefined;
+
+    // store the item at the front of the queue
+    var item = queue[offset];
+
+    // increment the offset and remove the free space if necessary
+    if (++ offset * 2 >= queue.length){
+      queue  = queue.slice(offset);
+      offset = 0;
+    }
+
+    // return the dequeued item
+    return item;
+
+  }
+
+  /* Returns the item at the front of the queue (without dequeuing it). If the
+   * queue is empty then undefined is returned.
+   */
+  this.peek = function(){
+    return (queue.length > 0 ? queue[offset] : undefined);
+  }
+
+}
+
 /* Exercise Code */
 class ExerciseMain {
 	constructor(ruleLength, initialState){
+		this._initialState = initialState.split("");
+	
 		this._rules = new Trie();
-		this._state = initialState.split("");
 		this._length = ruleLength;
 		
 		this._extraSpace = ruleLength + 1; // space to add at the front & end of the array
 		
-		this._lowerBound = 0;
-		this._upperBound = this._state.length;
+		this.setStartingValues();
 	}
+	
+	// NOTE: once this ends, the increments will all be equal - so just return the first element
+	get increment(){ return this._increments.peek(); }
 	
 	get plantPots(){
 		let result = [ ];
@@ -108,13 +178,22 @@ class ExerciseMain {
 		}
 		return result;
 	}
+	get plantPotsSum(){
+		let plantPots = this.plantPots;
+		let sum = 0;
+		
+		for (let index of plantPots)
+			sum += parseInt(index);
+		
+		return sum;
+	}
 	
 	addRule(rule, result){
 		this._rules.add(rule, result);
 	}
 	
-	next(){
-		let newState = { };
+	next(stateIndex){
+		let newState = [ ]; 
 		
 		this._lowerBound -= this._extraSpace;
 		this._upperBound += this._extraSpace;
@@ -123,12 +202,20 @@ class ExerciseMain {
 		
 		for (let i = this._lowerBound; i < this._upperBound; i++){
 			let result = this._getResult((index)=>this._get(index + i - offset));
-			if (result === PLANT_POT)
-				newState[i] = result;
+			newState[i] = result;
 		}
 		
-		this._state = newState;
-		return newState;
+		return this._setState(newState, stateIndex);
+	}
+	
+	setStartingValues(){
+		this._state = this._initialState;
+		
+		this._increments = new Queue();
+		this._lastPotValue = 0;
+		
+		this._lowerBound = 0;
+		this._upperBound = this._state.length;
 	}
 	
 	_get(index){
@@ -141,6 +228,31 @@ class ExerciseMain {
 		if (result === null)
 			return EMPTY_POT;
 		return result;
+	}
+	
+	_setState(state, index){
+		this._state = state;
+		return this._isRepeating(index);
+	}
+	
+	_isRepeating(index){
+		let newPots = this.plantPotsSum;
+		let increment = newPots - this._lastPotValue;
+		
+		if (this._checkRepetition(increment)) {
+			console.log(`Increasing by ${increment} at index ${index}`);
+			return true;
+		}
+		
+		this._lastPotValue = newPots;
+	}
+	_checkRepetition(increment){
+		let hasReachedMax = (this._increments.getLength() > NUM_REPETITIONS);
+		if (hasReachedMax)
+			this._increments.dequeue();
+		
+		this._increments.enqueue(increment);
+		return hasReachedMax && this._increments.isEquivalent();
 	}
 }
 
@@ -205,19 +317,36 @@ class UI extends UIBase {
 		this._init();
 	}
 	
-	run(){
-		this._exercise.next();
+	runInfinite(finalIteration){
+		let index = 0;
+		let hasReachedGoal = false;
+		
+		this._exercise.setStartingValues();
+		while (!hasReachedGoal) {
+			hasReachedGoal = this._exercise.next(index);
+			index++;
+		}
+		
+		if (DEBUG){
+			console.log(`Reached goal on iteration ${index}`);
+			console.log(`Increasing by ${this._exercise.increment}, current value ${this.plantsSum}`);
+		}
+		
+		let indexDifference = (finalIteration - index);
+		let totalIncrement = this._exercise.increment * indexDifference;
+		let finalValue = totalIncrement + this.plantsSum;
+		
+		return finalValue;
 	}
 	
-	get plantsSum(){
-		let plantPots = this._exercise.plantPots;
-		let sum = 0;
-		
-		for (let index of plantPots)
-			sum += parseInt(index);
-		
-		return sum;
+	run(numTimes){
+		this._exercise.setStartingValues();
+		for (let i = 0; i < numTimes; i++)
+			this._exercise.next(i);
+		return this.plantsSum;
 	}
+	
+	get plantsSum(){ return this._exercise.plantPotsSum; }
 	
 	_parse(line, lineNumber){
 		let [str, rule, result] = line.match(/([^ ]+) => (.)/);
@@ -234,6 +363,9 @@ class UI extends UIBase {
 
 /* Running */
 let ui = new UI(document.body.innerText);
-for (let i = 0; i < NUM_ITERATIONS; i++)
-	ui.run();
-console.log(ui.plantsSum);
+
+let resultPart1 = ui.run(NUM_ITERATIONS_P1);
+let resultPart2 = ui.runInfinite(NUM_ITERATIONS_P2);
+
+console.log(`Result Part 1: ${resultPart1}`);
+console.log(`Result Part 2: ${resultPart2}`);
