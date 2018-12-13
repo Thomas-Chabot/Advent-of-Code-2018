67ed6554ef0306508be79f05a04ad21d4184a12f
Thomas <seanhodges@cmail.carleton.ca>
2018-12-13 11:14:52 -0500

    Fixed row & column definitions
    Had rows & columns backwards, now been updated

--- Library/Grid.js
+++ Library/Grid.js
@@ -1,3 +1,4 @@
+
 class Grid {
 	constructor(numRows, numColumns, getValueFunction){
 		if (typeof(getValueFunction) !== "function"){
@@ -17,19 +18,19 @@ class Grid {
 	get numColumns(){ return this._numColumns; }
 	get numRows(){ return this._numRows; }
 	
-	get(row, col){
-		return this._takeAction(row, col, (column)=>{
-			return column[row];
+	get(rowIndex, colIndex){
+		return this._takeAction(rowIndex, colIndex, (row)=>{
+			return row[colIndex];
 		});
 	}
-	set(row, col, data){
-		return this._takeAction(row, col, (column)=>{
-			column[row] = data;
+	set(rowIndex, colIndex, data){
+		return this._takeAction(rowIndex, colIndex, (row)=>{
+			row[colIndex] = data;
 			return true;
 		});
 	}
-	reset(row, col){
-		return this._set(row, col, this._getValue(row, col));
+	reset(row, colIndex){
+		return this.set(row, colIndex, this._getValue(row, colIndex));
 	}
 	
 	clone(){
@@ -44,9 +45,9 @@ class Grid {
 		let nRows = this._numRows;
 		let nCols = this._numColumns;
 		
-		for (let col = 0; col < nCols; col++){
-			f(0, col, true);
-			for (let row = 1; row < nRows; row++){
+		for (let row = 0; row < nRows; row++){
+			f(row, 0, true);
+			for (let col = 1; col < nCols; col++){
 				f(row, col, false);
 			}
 		}
@@ -65,27 +66,44 @@ class Grid {
 	_init(){
 		this.each((row,col,isNextColumn)=>{
 			if (isNextColumn)
-				this._grid[col] = [ ];
-			this._grid[col][row] = this._getValue(row, col);
+				this._grid[row] = [ ];
+			this._grid[row][col] = this._getValue(row, col);
 		});
 	}
 	
-	_takeAction(row, col, f) {
-		if (!this._borderCheck(row, col)) return false;
+	_takeAction(rowIndex, colIndex, f) {
+		if (!this._borderCheck(rowIndex, colIndex)) return false;
 	
-		let column = this._column(col);
-		if (!column) return false;
+		let row = this._row(rowIndex);
+		if (!row) return false;
 		
-		return f(column);
+		return f(row);
 	}
-	_column(colNum, canCreate){
-		if (!this._grid[colNum] && canCreate)
-			this._grid[colNum] = [ ];
+	_row(rowNum, canCreate){
+		if (!this._grid[rowNum] && canCreate)
+			this._grid[rowNum] = [ ];
 		
-		return this._grid[colNum];
+		return this._grid[rowNum];
 	}
 	_borderCheck(row, col){
 		return (row >= 0 && row <= this._numRows
 			 && col >= 0 && col <= this._numColumns);
 	}
 }
+
+
+// Testing Code
+let grid = new Grid(5, 6, true);
+console.log (grid.toString());
+console.log(grid.numRows === 5);
+console.log(grid.numColumns === 6);
+
+let grid2 = grid.clone();
+console.log(grid2.toString() === grid.toString());
+
+grid2.set(1, 1, false);
+console.log(grid2.toString() !== grid.toString());
+console.log(!grid2.get(1,1));
+
+grid2.reset(1,1);
+console.log(grid2.get(1,1));
