7fc183fc71192a715c6e6971677c43cc383a872c
Thomas <seanhodges@cmail.carleton.ca>
2018-12-14 13:15:19 -0500

    Update Queue.src.js
    Changed the Queue class to use the ES6 Class definition

--- Library/DataStructures/Queue.src.js
+++ Library/DataStructures/Queue.src.js
@@ -1,81 +1,57 @@
-
-function Queue(){
-
-  // initialise the queue and offset
-  var queue  = [];
-  var offset = 0;
-
-  // Returns the length of the queue.
-  this.getLength = function(){
-    return (queue.length - offset);
-  }
-
-  // Returns true if the queue is empty, and false otherwise.
-  this.isEmpty = function(){
-    return (queue.length == 0);
-  }
-
-  /* Enqueues the specified item. The parameter is:
-   *
-   * item - the item to enqueue
-   */
-  this.enqueue = function(item){
-    queue.push(item);
-  }
-
-  /* Dequeues an item and returns it. If the queue is empty, the value
-   * 'undefined' is returned.
-   */
-  this.dequeue = function(){
-
-    // if the queue is empty, return immediately
-    if (queue.length == 0) return undefined;
-
-    // store the item at the front of the queue
-    var item = queue[offset];
-
-    // increment the offset and remove the free space if necessary
-    if (++ offset * 2 >= queue.length){
-      queue  = queue.slice(offset);
-      offset = 0;
-    }
-
-    // return the dequeued item
-    return item;
-
-  }
-
-  /* Returns the item at the front of the queue (without dequeuing it). If the
-   * queue is empty then undefined is returned.
-   */
-  this.peek = function(){
-    return (queue.length > 0 ? queue[offset] : undefined);
-  }
-  
-  /* Returns true if the elements in the queue matches the elements
-   * in the given array
-   */
-  this.equals = function(arr){
-	let index = 0;
-	for (let element of this){
-		if (element !== arr[index])
-			return false;
-		index ++;
+class Queue{
+	constructor(){
+		this._queue = [ ];
+		this._offset = 0;
 	}
-	return index === arr.length;
-  }
+	
+	getLength(){ return this._queue.length - this._offset; }
+	isEmpty(){ return this.getLength() === 0; }
+	enqueue(item){ this._queue.push(item); }
+	
+	peek(){
+		return (this._queue.length > 0 ? queue[offset] : undefined);
+	}
   
-  /* Returns an iterator so that the Queue can be looped through */
-  this[Symbol.iterator] = function(){
-	let index = -1;
-	return {
-		next: ()=>{
+	dequeue(){
+		// if the queue is empty, return immediately
+		if (this._queue.length == 0) return undefined;
+
+		// store the item at the front of the queue
+		var item = this._queue[this._offset];
+
+		// increment the offset and remove the free space if necessary
+		if (++ this._offset * 2 >= this._queue.length){
+		  this._queue  = this._queue.slice(this._offset);
+		  this._offset = 0;
+		}
+
+		// return the dequeued item
+		return item;
+	}
+	
+	equals(arr){
+		let index = 0;
+		for (let element of this){
+			if (element !== arr[index])
+				return false;
 			index ++;
-			return {
-				value: queue [offset + index],
-				done: offset + index === queue.length
-			};
+		}
+		return index === arr.length;
+	}
+	
+    [Symbol.iterator](){
+		let index = -1;
+		let queue = this._queue;
+		let offset = this._offset;
+		
+		return {
+			next: ()=>{
+				index ++;
+				return {
+					value: queue [offset + index],
+					done: offset + index === queue.length
+				};
+			}
 		}
 	}
-  }
 }
