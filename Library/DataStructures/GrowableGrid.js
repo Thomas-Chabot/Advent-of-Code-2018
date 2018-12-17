4d3ba986c60adc9deee4de12dd25e581e9e61e7e
Thomas <seanhodges@cmail.carleton.ca>
2018-12-17 16:31:47 -0500

    Update GrowableGrid.js
    The get() operation will only run on the bounds of the Grid. set() can still be run on any index & will add the new rows / columns. Border checking, etc. has been readded

--- Library/DataStructures/GrowableGrid.js
+++ Library/DataStructures/GrowableGrid.js
@@ -1,44 +1,48 @@
 let Grid = require ("./Grid.js");
 
 class GrowableGrid extends Grid {
-  constructor(getValueFunc){
-    super(-9e9, -9e9, getValueFunc);
+  constructor(getValueFunction){
+    super(-9e9, -9e9, getValueFunction);
 
     this._baseRow = this._baseColumn = 9e9;
   }
 
+  set(rowIndex, colIndex, value){
+    this._updateRowColTotals(rowIndex, colIndex);
+    return super.set(rowIndex, colIndex, value);
+  }
   _init(){
     return null;
   }
 
   _valueOf(row, columnIndex){
-    console.log(row, columnIndex, row[columnIndex]);
     if (row[columnIndex]) return row[columnIndex];
-
     return this._setValue(row, columnIndex, this._getValue(row._rowIndex, columnIndex));
   }
-  _setValue(row, columnIndex, value){
-    this._numColumns = Math.max(this._numColumns, columnIndex+1);
-    this._baseColumn = Math.min(this._baseColumn, columnIndex);
 
-    row[columnIndex] = value;
-    return value;
-  }
+  _takeAction(rowIndex, colIndex, isSetOperation, f){
+    let canCreate = isSetOperation || super._borderCheck(rowIndex, colIndex);
 
-  _takeAction(rowIndex, colIndex, f){
-    let row = this._row(rowIndex);
+    let row = this._row(rowIndex, canCreate);
+    if (!row) return false;
+
     return f(row);
   }
-  _borderCheck(r, c){
-    return true;
-  }
-  _row(rowNum){
-    let row = super._row(rowNum, true);
+  _row(rowNum, canCreate){
+    let row = super._row(rowNum, canCreate);
+    if (!row) return;
+
     row._rowIndex = rowNum;
-
-    this._numRows = Math.max(this._numRows, rowNum+1);
-    this._baseRow = Math.min(this._baseRow, rowNum);
-
     return row;
   }
+
+  _updateRowColTotals(rowIndex, columnIndex){
+    this._numRows = Math.max(this._numRows, rowIndex+1);
+    this._numColumns = Math.max(this._numColumns, columnIndex+1);
+
+    this._baseRow = Math.min(this._baseRow, rowIndex);
+    this._baseColumn = Math.min(this._baseColumn, columnIndex);
+  }
 }
+
+module.exports = GrowableGrid;
