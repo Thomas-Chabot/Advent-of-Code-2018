79a1b32a48803fa8ed4a22dd713d648870539512
Thomas <seanhodges@cmail.carleton.ca>
2018-12-20 15:01:09 -0500

    Update Image.js

--- Library/Modules/Image.js
+++ Library/Modules/Image.js
@@ -6,26 +6,32 @@
       Arguments:
         outputPath : String : The path to where the file should be saved
         text       : String : The data to write. Should be made up of two characters.
-        writeChar  : String : The character to use for writing. Will put a black dot wherever this character appears;
-                                all other data will be filled in as white.
+        characterColors  : Dictionary : Colors to use for each character in the string.
+                                        Should have an additional property, default, to be used
+                                          in case the character was not specified.
 
 */
 
 let jimp = require("jimp");
 class Image {
   // NOTE: This is reliant on all lines being of the same length
-  static writeText(outputPath, text, writeCharacter){
+  static writeText(outputPath, text, characterColors){
+    if (typeof (characterColors) === "string")
+      characterColors = {
+        characterColors: [0, 0, 0],
+        default: [255, 255, 255]
+      };
+
     let lines = text.split("\n");
     let lengthY = lines.length;
     let lengthX = lines[0] === undefined ? 0 : lines[0].length;
-    console.log(lengthX);
 
     let image = new jimp(lengthX, lengthY);
     for (let y = 0; y < lines.length; y++) {
       lines[y] = lines[y].split(""); // make it into an array - easier to check characters
       for (let x = 0; x < lines[y].length; x++) {
         let char = lines[y][x];
-        let color = this._getColorOf(char, writeCharacter);
+        let color = this._getColorOf(char, characterColors);
 
         image.setPixelColor(color, x, y);
       }
@@ -34,10 +40,11 @@ class Image {
     return image.writeAsync(outputPath);
   }
 
-  static _getColorOf(character, writeCharacter){
-    if (character === writeCharacter)
-      return jimp.rgbaToInt(0, 0, 0, 255);
-    return jimp.rgbaToInt(255, 255, 255, 255);
+  static _getColorOf(character, colorsTable){
+    if (!colorsTable[character])
+      character = "default";
+
+    return jimp.rgbaToInt(...colorsTable[character]);
   }
 }
 
