ecfd78c5578307027d1cb30e68ec47d1dcd102e7
Thomas <seanhodges@cmail.carleton.ca>
2018-12-20 14:58:08 -0500

    Update main.js
    Exercise updates

--- Days/Day13/main.js
+++ Days/Day13/main.js
@@ -1,15 +1,30 @@
 let library = "../../Library/Modules";
-let input = "./input/input.txt";
-let output = "./output";
+let input = "./input/example.txt";
+let output = "./output/out{INDEX}.txt";
 
 let File = require (library + "/File.js");
+let Image = require (library + "/Image.js");
 let UI   = require ("./modules/UI.js");
+let Constants = require ("./modules/Constants.js");
+
+let {TYPE_CART, TYPE_WALL, TYPE_FLOOR} = Constants;
+
+let imageColors = {
+  [TYPE_CART]: [255, 0, 0, 255],
+  [TYPE_FLOOR]: [0, 0, 0, 255],
+  [TYPE_WALL]: [255, 255, 255, 255]
+}
 
 File.read(input).then((data)=>{
   let ui = new UI(data);
-  console.log(ui.grid.toString());
+  let index = 0;
 
-  ui.run();
+  ui.run((grid)=>{
+    index++;
+
+    let filePath = output.replace("{INDEX}", index);
+    return Image.write(filePath, grid, imageColors);
+  });
 
   // exercise result code here
 }, (err)=>console.log(err));
